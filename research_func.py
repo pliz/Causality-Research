7d6ae50d4f463e03939a6023976a2984953b1061
Cynthia Wu <cynthiaw2004@gmail.com>
2015-02-18 20:08:12 -0700

    testing

--- research_func.py
+++ research_func.py
@@ -160,11 +160,11 @@ def findAllGraphs(g, gu, rate):
 
 #gu to g1 algorithm
 #NOT YET WORKABLE
-def gutog1(h):
+def gutog1(h,max_u):
 
     G_test = copy.deepcopy(h)
-    for edge in edgelist(h):
-        delanedge(G_test,edge)
+    for edge in tool.edgelist(h):
+        tool.delanedge(G_test,edge)
     #G_test is h with all edges removed
 
     G1_found = False
@@ -172,12 +172,12 @@ def gutog1(h):
 
     while G1_found == False:
         #initialization for edgetuples with only 1 edge
-        initialedges = tool.edgelist(tool.superclique(h))
+        initialedges = tool.edgelist(tool.superclique(tool.numofvertices(h)))
         edgesleft = copy.deepcopy(initialedges)   
         #when an edgetuple consists of only 1 edge, it needs to be handled individually
         for edge in initialedges:
             tool.addanedge(G_test,edge)
-            testgu =  tool.undersample(G_test,test_u)
+            testgu =  tool.undersample(G_test,u)
             if testgu == h:
                 G1 = copy.deepcopy(G_test)
                 u1 = u
@@ -188,11 +188,9 @@ def gutog1(h):
                 edgesleft.remove(edge)
 
         if G1_found == True:
+            print (G1,u1)
             return (G1, u1)
             break
-
-        ####THIS FAR####
-
 
         #initialization for edgetuples with 2+edges
         edgetuples = []
@@ -201,57 +199,35 @@ def gutog1(h):
             edgetuples.append(comb)
             edgetuplesleft.append(comb)
         #when an edgetuple consists of 2+ edges
-        for i in range(2,tool.numofvertices(g)+1):
+        for i in range(2,tool.numofvertices(h)+1):
             for edgetuple in edgetuples:
                 for edge in edgetuple:
-                    tool.addanedge(g,edge)
-                testgu = tool.undersample(g,rate)
-                if testgu == gu:
-                    addg = copy.deepcopy(g)
-                    s.append(addg)
-                    tool.delanedge(g,edge)
+                    tool.addanedge(G_test,edge)
+                testgu = tool.undersample(G_test,u)
+                if testgu == h:
+                    G1 = copy.deepcopy(G_test)
+                    u1 = u
+                    print (G1,u1)
+                    return (G1,u1)
+                    G1_found = True
+                    tool.delanedge(G_test,edge)
                 else:
                     edgetuplesleft.remove(edgetuple)     
                 for edge in edgetuple:
-                    tool.delanedge(g,edge)
+                    tool.delanedge(G_test,edge)
             if not edgetuplesleft:  
                 break
             else:
-                oldedgetuples = edgetuplesleft
-                edgetuples = []
-                edgetuplesleft = []
-                for comb in combinations(oldedgetuples,i+1):
-                    edgetuples.append(comb)
-                    edgetuplesleft.append(comb)
-        s1 = set()  #want to return a set of supergraphs
-        for graph in s:
-            s1.add(tool.g2num(graph))
-        #s contains dictionary graph notation
-        #s1 contains string graph notation
-        return (s,s1)
-
-
+                edgetuples = tool.createNextEi(edgetuples,i)
+                edgetuplesleft = copy.deepcopy(edgetuples) 
+        u = u + 1
+        if u == max_u:
+            print "no G found given max_u"
+            break
         
-
-
 def main():
 
     #TESTING SUPERGRAPH
-
-    #from page 3 of danks and plis paper
-    #g = {
-    #'1': {'1': set([(0, 1)]), '2': set([(0, 1)])},
-    #'2': {'3': set([(0, 1)])},
-    #'3': {'4': set([(0, 1)])},
-    #'4': {'1': set([(0, 1)])}
-    #}
-    #g2 = {
-    #'1': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)])},
-    #'3': {'1': set([(0, 1)])}, 
-    #'2': {'1': set([(2, 0)]), '4': set([(0, 1)])}, 
-    #'4': {'1': set([(0, 1)]), '2': set([(0, 1)])}
-    #}
-    #supergraphs_in_eq(g, g2, 1) #no supergraphs!
 
     #from email "a task for you"
     #g = {
@@ -391,6 +367,31 @@ def main():
     #findminG1(g,g3,2)   #returns h1
     #findEquivClass(g, g3, 2)
 
+
+
+
+    #TESTING GU TO G1 ALG
+
+    #G is true causal structure
+    G = {
+    '1': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)])},
+    '3': {'1': set([(0, 1)])}, 
+    '2': {'1': set([(2, 0)]), '4': set([(0, 1)])}, 
+    '4': {'1': set([(0, 1)]), '2': set([(0, 1)])}
+    }
+
+    #G2 is the undersampled structure
+    G2 = tool.undersample(g, 1)
+
+    G2 = {
+    '1': {'1': set([(0, 1)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1), (2, 0)]), '4': set([(0, 1)])}, 
+    '3': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)])}, 
+    '2': {'1': set([(0, 1), (2, 0)]), '3': set([(2, 0)]), '2': set([(0, 1)])}, 
+    '4': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)]), '4': set([(0, 1)])}
+    }
+    
+    max_u=10
+    gutog1(G2,max_u) #should give G but isn't....
 
 
 if __name__ == "__main__":
