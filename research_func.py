4fc627d63fae60ecf58126daa01d4ee4f6520ca5
Sergey Plis <s.m.plis@gmail.com>
2015-03-04 13:27:19 -0700

    *** empty log message ***

--- research_func.py
+++ research_func.py
@@ -1,6 +1,7 @@
 #BFS implementation of subgraph and supergraph
 #Gu to G1 algorithm
 from itertools import combinations
+from functools import wraps
 import copy
 import time
 
@@ -9,12 +10,6 @@ TOOLSPATH='~/Users/cynthia/Desktop/Causality/Causality-Research/mytools.py'
 sys.path.append(os.path.expanduser(TOOLSPATH))
 
 import mytools as tool
-
-
-
-
-
-
 
 #returns all the supergraphs of g that are in the same equivalence class
 #as g with respect to gu and the rate
@@ -29,7 +24,7 @@ def supergraphs_in_eq(g, gu, rate):
 
     #initialization for edgetuples with only 1 edge
     initialedges = tool.edgelist(tool.complement(g))
-    edgesleft = copy.deepcopy(initialedges)   
+    edgesleft = copy.deepcopy(initialedges)
     #when an edgetuple consists of only 1 edge, it needs to be handled individually
     for edge in initialedges:
         tool.addanedge(g,edge)
@@ -59,14 +54,14 @@ def supergraphs_in_eq(g, gu, rate):
                 s.append(addg)
                 tool.delanedge(g,edge)
             else:
-                edgetuplesleft.remove(edgetuple)     
+                edgetuplesleft.remove(edgetuple)
             for edge in edgetuple:
                 tool.delanedge(g,edge)
-        if not edgetuplesleft:  
+        if not edgetuplesleft:
             break
         else:
             edgetuples = tool.createNextEi2(edgetuples,i)
-            edgetuplesleft = copy.deepcopy(edgetuples) 
+            edgetuplesleft = copy.deepcopy(edgetuples)
 
     s1 = set()  #want to return a set of supergraphs
     for graph in s:
@@ -75,11 +70,6 @@ def supergraphs_in_eq(g, gu, rate):
     #s1 contains string graph notation
     print (s,s1)
     return (s,s1)
-
-
-
-
-
 
 
 #returns all the subgraphs of g that are in the same equivalence class
@@ -95,7 +85,7 @@ def subgraphs_in_eq(g, gu, rate):
 
     #initialization for edgetuples with only 1 edge
     initialedges = tool.edgelist(g)
-    edgesleft = copy.deepcopy(initialedges)   
+    edgesleft = copy.deepcopy(initialedges)
     #when an edgetuple consists of only 1 edge, it needs to be handled individually
     for edge in initialedges:
         tool.delanedge(g,edge)
@@ -124,14 +114,14 @@ def subgraphs_in_eq(g, gu, rate):
                 addg = copy.deepcopy(g)
                 s.append(addg)
             else:
-                edgetuplesleft.remove(edgetuple)     
+                edgetuplesleft.remove(edgetuple)
             for edge in edgetuple:
                 tool.addanedge(g,edge)
-        if not edgetuplesleft:  
+        if not edgetuplesleft:
             break
         else:
             edgetuples = tool.createNextEi2(edgetuples,i)
-            edgetuplesleft = copy.deepcopy(edgetuples) 
+            edgetuplesleft = copy.deepcopy(edgetuples)
     s1 = set()  #want to return a set of subgraphs
     for graph in s:
         s1.add(tool.g2num(graph))
@@ -171,7 +161,7 @@ def explodinggutog1(h,max_u):
     while G1_found == False:
         #initialization for edgetuples with only 1 edge
         initialedges = tool.edgelist(tool.superclique(tool.numofvertices(h)))
-        edgesleft = copy.deepcopy(initialedges)   
+        edgesleft = copy.deepcopy(initialedges)
         #when an edgetuple consists of only 1 edge, it needs to be handled individually
         for edge in initialedges:
             print "adding edge ", edge
@@ -214,7 +204,7 @@ def explodinggutog1(h,max_u):
                     return (G1,u1)
                     G1_found = True
                     tool.delanedge(G_test,edge)
-                else:     
+                else:
                     print "fails for u = ",u
                     print "\n"
                     for edge in edgetuple:
@@ -223,11 +213,11 @@ def explodinggutog1(h,max_u):
                     print "testgu has hit superclique"
                     #should I force it to move to the next u?
                     #testing to find out
-            if not edgetuplesleft:  
+            if not edgetuplesleft:
                 break
             else:
                 edgetuples = tool.createNextEi1(edgetuples,i)
-                edgetuplesleft = copy.deepcopy(edgetuples) 
+                edgetuplesleft = copy.deepcopy(edgetuples)
         u = u + 1
         if u == max_u:
             print "no G found given max_u"
@@ -263,7 +253,7 @@ def search(G,H,EL,S,newEls,edgesinG):
                 newEl.append(edge)
             tool.delanedge(G,edge)
         #we have now constructed newEl
-        newEl1 = copy.deepcopy(newEl) 
+        newEl1 = copy.deepcopy(newEl)
 
         #print "G: ",tool.edgelist(G)
         #print "current new El: ",newEl
@@ -282,7 +272,7 @@ def search(G,H,EL,S,newEls,edgesinG):
                 edgesinG.remove(edgesinG[-1])
                 newEls.remove(newEls[-1])
                 newEls[-1].remove(newEls[-1][-1])
-        
+
         for edge in newEl1:
             tool.addanedge(G,edge)
             edgesinG.append(edge)
@@ -292,7 +282,7 @@ def search(G,H,EL,S,newEls,edgesinG):
                 #print "G1 found: ",G
                 S.add(tool.gsig(G))
                 return S
-                
+
 
             Gedges = tool.edgelist(G)
             #anotherEl consists of all the edges that Gedges does not have
@@ -300,25 +290,105 @@ def search(G,H,EL,S,newEls,edgesinG):
             for gedge in Gedges:
                 if gedge in anotherEl:
                     anotherEl.remove(gedge)
-            
+
             search(G,H,anotherEl,S,newEls,edgesinG)
 
-    else:  #execute this statement if EL is empty(no more edges can be added because we have hit the superclique)       
+    else:  #execute this statement if EL is empty(no more edges can be added because we have hit the superclique)
         return None
-   
 
-            
+
+
 #hopeful gu to g1 algorithm
-#adding edge by edge depth first search   
+#adding edge by edge depth first search
 def hopefulgutog1(H):
-        G = tool.cloneempty(H)
-        EL = tool.edgelist(tool.superclique(tool.numofvertices(H)))
-        S = set()
-        return search(G,H,EL,S,[],[])
-        
-        
+    G = tool.cloneempty(H)
+    EL = tool.edgelist(tool.superclique(tool.numofvertices(H)))
+    S = set()
+    def search(G,H,EL,S,newEls,edgesinG):
+        newEl = []
+        if EL:  #execute this statement if EL is not empty
+            for edge in EL:
+                tool.addanedge(G,edge)
+                if not tool.checkconflict(H,G):
+                    newEl.append(edge)
+                tool.delanedge(G,edge)
+            newEl1 = copy.deepcopy(newEl)
+
+            if newEl: #execute if newEl is not empty
+                newEls.append(newEl)
+            if tool.edgelist(G):
+                if not newEl:
+                    tool.delanedge(G,edgesinG[-1])
+                    edgesinG.remove(edgesinG[-1])
+                    newEls[-1].remove(newEls[-1][-1])
+                while not newEls[-1]:
+                    if newEls == [[]]:
+                        return None
+                    tool.delanedge(G,edgesinG[-1])
+                    edgesinG.remove(edgesinG[-1])
+                    newEls.remove(newEls[-1])
+                    newEls[-1].remove(newEls[-1][-1])
+
+            for edge in newEl1:
+                tool.addanedge(G,edge)
+                edgesinG.append(edge)
+
+                if tool.checkequality(H,G): S.add(tool.gsig(G))
+
+                anotherEl = tool.edgelist(tool.complement(G))
+
+                search(G,H,anotherEl,S,newEls,edgesinG)
+
+            else:
+                return None
+
+    search(G,H,EL,S,[],[])
+    return S
 
 
+def memo(func):
+    cache = {}                        # Stored subproblem solutions
+    @wraps(func)                      # Make wrap look like func
+    def wrap(*args):                  # The memoized wrapper
+        s = tool.signature(args[0],args[2])# Signature: g and edges
+        if s not in cache:            # Not already computed?
+            cache[s] = func(*args)    # Compute & cache the solution
+        return cache[s]               # Return the cached solution
+    return wrap
+
+
+def eqclass(H):
+    '''
+    Find all graphs in the same equivalence class with respect to
+    graph H and any undesampling rate.
+    '''
+    g = {n:{} for n in H}
+    s = set()
+    @memo
+    def addedges(g,H,edges):
+        if edges:
+            masks  = []
+            for e in edges:
+                tool.addanedge(g,e)
+                if tool.checkconflict(H,g):
+                    masks.append(False)
+                else:
+                    masks.append(True)
+                tool.delanedge(g,e)
+
+            nedges = [edges[i] for i in range(len(edges)) if masks[i]]
+            n = len(nedges)
+            if n:
+                for i in range(n):
+                    tool.addanedge(g,nedges[i])
+                    if tool.checkequality(H,g):
+                        s.add(tool.gsig(g))
+                    addedges(g,H,nedges[:i]+nedges[i+1:])
+                    tool.delanedge(g,nedges[i])
+
+    edges = tool.edgelist(tool.complement(g))
+    addedges(g,H,edges)
+    return s
 
 
 
@@ -339,7 +409,7 @@ def main():
     #'8': {'1': set([(0, 1)]),'3': set([(0, 1)]),'4': set([(0, 1)]),'7': set([(0, 1)]),'8': set([(0, 1)])}
     #}
     #g2 = tool.undersample(g, 1)
-    #h is a supergraph in the equivalence class of g with the extra edge (5,7) 
+    #h is a supergraph in the equivalence class of g with the extra edge (5,7)
     #h = {
     #'1': {'2': set([(0, 1)]), '4': set([(0, 1)]), '7': set([(0, 1)])},
     #'2': {'3': set([(0, 1)]), '4': set([(0, 1)]), '7': set([(0, 1)])},
@@ -413,7 +483,7 @@ def main():
     #'7': {'8': set([(0, 1)])},
     #'8': {'1': set([(0, 1)]),'3': set([(0, 1)]),'4': set([(0, 1)]),'7': set([(0, 1)]),'8': set([(0, 1)])}
     #}
-    #h is a subgraph in the equivalence class of g with the extra edge (5,7) 
+    #h is a subgraph in the equivalence class of g with the extra edge (5,7)
     #h = {
     #'1': {'2': set([(0, 1)]), '4': set([(0, 1)]), '7': set([(0, 1)])},
     #'2': {'3': set([(0, 1)]), '4': set([(0, 1)]), '7': set([(0, 1)])},
@@ -476,9 +546,9 @@ def main():
     #TESTING strawman GU to G1 ALG
     #random
     #H = {
-    #'1': {'1': set([(0, 1)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1), (2, 0)]), '4': set([(0, 1)])}, 
-    #'3': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)])}, 
-    #'2': {'1': set([(0, 1), (2, 0)]), '3': set([(2, 0)]), '2': set([(0, 1)])}, 
+    #'1': {'1': set([(0, 1)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1), (2, 0)]), '4': set([(0, 1)])},
+    #'3': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)])},
+    #'2': {'1': set([(0, 1), (2, 0)]), '3': set([(2, 0)]), '2': set([(0, 1)])},
     #'4': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)]), '4': set([(0, 1)])}
     #}
     #max_u=5
@@ -500,13 +570,13 @@ def main():
 
     #random
     #H = {
-    #'1': {'1': set([(0, 1)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1), (2, 0)]), '4': set([(0, 1)])}, 
-    #'3': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)])}, 
-    #'2': {'1': set([(0, 1), (2, 0)]), '3': set([(2, 0)]), '2': set([(0, 1)])}, 
+    #'1': {'1': set([(0, 1)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1), (2, 0)]), '4': set([(0, 1)])},
+    #'3': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)])},
+    #'2': {'1': set([(0, 1), (2, 0)]), '3': set([(2, 0)]), '2': set([(0, 1)])},
     #'4': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)]), '4': set([(0, 1)])}
     #}
     #max_u=5
-    #(G,u) = explodinggutog1(H,max_u) #find a G such that H = G^2 
+    #(G,u) = explodinggutog1(H,max_u) #find a G such that H = G^2
     #print "graph is: ",G," and u is: ",u
     #print "in hash notation, G is ",tool.g2num(G)
     #does this G satisfy G^2=H?
@@ -515,21 +585,21 @@ def main():
     #    print "yes"
 
     #H= {
-    #'1': {'3': set([(2, 0)]), '2': set([(0, 1), (2, 0)]), '5': set([(0, 1), (2, 0)]), '4': set([(0, 1), (2, 0)])}, 
-    #'3': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)]), '5': set([(0, 1), (2, 0)]), '4': set([(0, 1), (2, 0)])}, 
-    #'2': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1)]), '5': set([(0, 1), (2, 0)]), '4': set([(0, 1), (2, 0)])}, 
-    #'5': {'1': set([(2, 0)]), '3': set([(0, 1), (2, 0)]), '2': set([(2, 0)]), '4': set([(0, 1), (2, 0)])}, 
+    #'1': {'3': set([(2, 0)]), '2': set([(0, 1), (2, 0)]), '5': set([(0, 1), (2, 0)]), '4': set([(0, 1), (2, 0)])},
+    #'3': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)]), '5': set([(0, 1), (2, 0)]), '4': set([(0, 1), (2, 0)])},
+    #'2': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1)]), '5': set([(0, 1), (2, 0)]), '4': set([(0, 1), (2, 0)])},
+    #'5': {'1': set([(2, 0)]), '3': set([(0, 1), (2, 0)]), '2': set([(2, 0)]), '4': set([(0, 1), (2, 0)])},
     #'4': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1), (2, 0)]), '5': set([(0, 1), (2, 0)]), '4': set([(0, 1)])}
     #}
     #max_u=2
-    #(G,u) = explodinggutog1(H,max_u) #find a G such that H = G^2 
+    #(G,u) = explodinggutog1(H,max_u) #find a G such that H = G^2
     #print "graph is: ",G," and u is: ",u
     #here is the G that is returned
     #G = {
-    #'1': {'4': set([(0, 1)])}, 
-    #'3': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)]), '5': set([(0, 1)]), '4': set([(0, 1)])}, 
-    #'2': {'3': set([(0, 1)])}, 
-    #'5': {'1': set([(0, 1)]), '2': set([(0, 1)])}, 
+    #'1': {'4': set([(0, 1)])},
+    #'3': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)]), '5': set([(0, 1)]), '4': set([(0, 1)])},
+    #'2': {'3': set([(0, 1)])},
+    #'5': {'1': set([(0, 1)]), '2': set([(0, 1)])},
     #'4': {'2': set([(0, 1)]), '5': set([(0, 1)]), '4': set([(0, 1)])}
     #}
     #does this G satisfy G^2=H?
@@ -550,29 +620,29 @@ def main():
     #TESTING HOPEFUL GU to G1 Algorithm
 
     H = {
-    '1': {'1': set([(0, 1)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1), (2, 0)]), '4': set([(0, 1)])}, 
-    '3': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)])}, 
-    '2': {'1': set([(0, 1), (2, 0)]), '3': set([(2, 0)]), '2': set([(0, 1)])}, 
+    '1': {'1': set([(0, 1)]), '3': set([(0, 1), (2, 0)]), '2': set([(0, 1), (2, 0)]), '4': set([(0, 1)])},
+    '3': {'1': set([(0, 1), (2, 0)]), '3': set([(0, 1)]), '2': set([(0, 1), (2, 0)])},
+    '2': {'1': set([(0, 1), (2, 0)]), '3': set([(2, 0)]), '2': set([(0, 1)])},
     '4': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)]), '4': set([(0, 1)])}
     }
 
     t0 = time.time()
-    print hopefulgutog1(H) 
+    print hopefulgutog1(H)
     t1 = time.time()
     total = t1-t0
     print "time in seconds: ", total
 
     #two G's are found
     #G = {
-    #'1': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)])}, 
-    #'3': {'1': set([(0, 1)]), '3': set([(0, 1)])}, 
-    #'2': {'4': set([(0, 1)])}, 
+    #'1': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)])},
+    #'3': {'1': set([(0, 1)]), '3': set([(0, 1)])},
+    #'2': {'4': set([(0, 1)])},
     #'4': {'1': set([(0, 1)]), '2': set([(0, 1)])}
     #}
     #G = {
-    #'1': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)])}, 
-    #'3': {'1': set([(0, 1)])}, 
-    #'2': {'4': set([(0, 1)])}, 
+    #'1': {'1': set([(0, 1)]), '3': set([(0, 1)]), '2': set([(0, 1)])},
+    #'3': {'1': set([(0, 1)])},
+    #'2': {'4': set([(0, 1)])},
     #'4': {'1': set([(0, 1)]), '2': set([(0, 1)])}
     #}
 
@@ -582,7 +652,7 @@ def main():
     #    print "yes"
 
     #H= {
-    #'1': {'2': set([(1, 0)])},  
+    #'1': {'2': set([(1, 0)])},
     #'2': {'1': set([(1, 0)]), '2': set([(1, 0)])},
     #'3': {'1': set([(1, 0)]), '2': set([(1, 0)])}
     #}
